5c120c02265a4955af857467b46bee869eb4c643
Rosangela Devilla <dev077@pearcey-login.cm.hpc>
2019-04-24 10:32:44 +1000

    split columns to tidy dataframe

--- scripts/Rscript/Rscript.R
+++ scripts/Rscript/Rscript.R
@@ -1,1 +1,16 @@
-#This is a R script
+#This is a R script for Big Data Assignment.
+
+library(tidyverse)
+library(readtext)
+
+#reading in the datafile
+datafile <- read_csv('/OSM/CBR/AF_DATASCHOOL/input/2019-04-12_Transcritome/R_161128_SHADIL_LIB2500_M002.csv',
+                    col_names = TRUE, skip = 14) 
+# this is the raw data being cleaned up into a tidy dataframe.
+print(datafile)
+
+datatidy_rosmay <- datafile %>%
+  separate_('Index', c("index_1", "index_2", 'index_3'), sep = '[ ]') %>%
+  glimpse(datatidy_rosmay)
+  print(datatidy_rosmay)         
+
