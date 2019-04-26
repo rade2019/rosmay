9b1e0cc73d37015b4a25632f78bee6a295d81a9d
Rosangela Devilla <dev077@pearcey-i3.cm.hpc>
2019-04-26 11:45:36 +1000

    data tidy almost complete

--- scripts/Rscript/Rscript.R
+++ scripts/Rscript/Rscript.R
@@ -2,6 +2,7 @@
 
 library(tidyverse)
 library(readtext)
+library(stringi)
 
 #reading in the datafile
 datafile <- read_csv('/OSM/CBR/AF_DATASCHOOL/input/2019-04-12_Transcritome/R_161128_SHADIL_LIB2500_M002.csv',
@@ -10,7 +11,10 @@ datafile <- read_csv('/OSM/CBR/AF_DATASCHOOL/input/2019-04-12_Transcritome/R_161
 print(datafile)
 
 datatidy_rosmay <- datafile %>%
-  separate_('Index', c("index_1", "index_2", 'index_3'), sep = '[ ]') %>%
+  separate(Index, into = c("index_1", "index_2","index_3", "index_4", "gen_1", "gen_2"))  %>%
+  separate("External ID", c("day_duration", "day_sample"), sep = 1) %>%
+  print(datatidy_rosmay)
   glimpse(datatidy_rosmay)
-  print(datatidy_rosmay)         
-
+  
+  
+write_csv(x = datatidy_rosmay, path = '/OSM/CBR/AF_DATASCHOOL/output/rosmay')
