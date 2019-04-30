8cd43a575039a01f986d06a499ecd5159c12d208
Tracey May <may159@pearcey-login.cm.hpc>
2019-04-30 14:21:29 +1000

    repair broken separate external id

--- scripts/Rscript/Rscript.R
+++ scripts/Rscript/Rscript.R
@@ -8,7 +8,8 @@ library(tidyverse)
 library(readtext)
 library(stringi)
 library(stringr)
- 
+
+setwd('/OSM/CBR/AF_DATASCHOOL/input/2019-04-12_Transcritome') 
 savePath <- ('/OSM/CBR/AF_DATASCHOOL/output/rosmay')
   
 #reading in the datafile
@@ -17,22 +18,27 @@ datafile <- read_csv('/OSM/CBR/AF_DATASCHOOL/input/2019-04-12_Transcritome/R_161
 
 # this is the raw data being cleaned up into a tidy dataframe.
 print(datafile)
+
   
 datatidy_rosmay <- datafile %>%
+  separate("External ID", into = c("day_duration", "day_sample"), sep = 1) %>%
   separate(Index, into = c("index_1", "index_2","gen"), sep = " ")  %>%
   mutate(gen = str_remove(gen, '\\(')) %>% 
-  mutate(gen = str_remove(gen, '\\)')) %>% 
-  separate("External ID", c("day_duration", "day_sample"), sep = 1) %>%
+  mutate(gen = str_remove(gen, '\\)'))
   
-write_csv(x = datatidy_out_csv,path = '/OSM/CBR/AF_DATASCHOOL/output/rosmay/datatidy_out.csv')
-write_tsv (x = datatidy_out_tsv,path = '/OSM/CBR/AF_DATASCHOOL/output/rosmay/datatidy_out.tsv')
+  
+write_csv(x = datatidy_rosmay,path = '/OSM/CBR/AF_DATASCHOOL/output/rosmay/datatidy_out.csv')
+write_tsv (x = datatidy_rosmay,path = '/OSM/CBR/AF_DATASCHOOL/output/rosmay/datatidy_out.tsv')
 read_csv('/OSM/CBR/AF_DATASCHOOL/output/rosmay/datatidy_out.tsv')
 
 # read in the file names and produce a table to join
 
 file_names <- list.files(path = '/OSM/CBR/AF_DATASCHOOL/input/2019-04-12_Transcritome', 
-                         full.names = T, pattern = "*.fastq.gz") %>% 
+                         full.names = T, pattern = "*.fastq.gz")
 
+fastq_list <- data_frame(filename = file_names) %>% 
+  mutate(file_contents = map(filename, ~ read_csv(file.path(getwd(), .),
+                                                            
 
 
 write_tsv(x = file_names, path = '/OSM/CBR/AF_DATASCHOOL/output/rosmay/filenames_out.tsv' )
