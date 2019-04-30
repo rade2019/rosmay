ee15827f7f7776eff473e18765c7b52eace38f82
Tracey May <may159@pearcey-login.cm.hpc>
2019-04-30 11:04:24 +1000

    Tidy dataframe, change separate criterea to use space, remove brackets from gene

--- scripts/Rscript/Rscript.R
+++ scripts/Rscript/Rscript.R
@@ -1,20 +1,32 @@
 #This is a R script for Big Data Assignment.
+#Create tidy dataframe for metadata csv
+
+#Clear global environment
+rm(list = ls())
 
 library(tidyverse)
 library(readtext)
 library(stringi)
-
+ 
+savePath <- ('/OSM/CBR/AF_DATASCHOOL/output/rosmay')
+  
 #reading in the datafile
 datafile <- read_csv('/OSM/CBR/AF_DATASCHOOL/input/2019-04-12_Transcritome/R_161128_SHADIL_LIB2500_M002.csv',
-                    col_names = TRUE, skip = 14) 
+                       col_names = TRUE, skip = 14) 
 # this is the raw data being cleaned up into a tidy dataframe.
 print(datafile)
-
+  
 datatidy_rosmay <- datafile %>%
-  separate(Index, into = c("index_1", "index_2","index_3", "index_4", "gen_1", "gen_2"))  %>%
+  separate(Index, into = c("index_1", "index_2","gen"), sep = " ")  %>%
+  mutate(gen = str_remove(gen, '\\(')) %>% 
+  mutate(gen = str_remove(gen, '\\)'))
+
+
   separate("External ID", c("day_duration", "day_sample"), sep = 1) %>%
   print(datatidy_rosmay)
-  glimpse(datatidy_rosmay)
   
   
-write_csv(x = datatidy_rosmay, path = '/OSM/CBR/AF_DATASCHOOL/output/rosmay')
+write_csv(x = datatidy_rosmay,path = '/OSM/CBR/AF_DATASCHOOL/output/rosmay/datatidy_out.csv')
+write_tsv (x = datatidy_rosmay,path = '/OSM/CBR/AF_DATASCHOOL/output/rosmay/datatidy_out.tsv')
+read_csv('/OSM/CBR/AF_DATASCHOOL/output/rosmay/datatidy_out.tsv')
+print(datatidy_rosmay)
